9b990f4c771cff7ae4538d58b80769446e9889d6
Alan Levicki <alevicki@gmail.com>
2017-08-09 11:11:28 -0500

    Add file type icons

--- src/components/BindingListItem.js
+++ src/components/BindingListItem.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {FormattedDate} from 'react-intl';
 
+import {Button} from 'react-bootstrap';
+
+import './BindingListItem.css';
+import FileTypeIcon from './icons/FileTypeIcon';
+
 class BindingsListItem extends Component {
 
   static propTypes = {
     file: PropTypes.object,
     binding: PropTypes.object,
     removeBinding: PropTypes.func,
+    addBinding: PropTypes.func,
     getFilename: PropTypes.func
   }
 
@@ -16,14 +22,21 @@ class BindingsListItem extends Component {
   }
 
   render () {
-    const { file, binding } = this.props;
+    const { addBinding, file, binding } = this.props;
     console.log(file);
+
+    const isBindable = true;
+
     return (
       <div className='file' key={file.id}>
+        <FileTypeIcon filename={file.name} />
         <div className='center-info'>
           <div className='title'>{file.name}</div>
           <div className='info'>{binding && `${binding.rangeAddress}&middot; `}Updated <FormattedDate value={file.updated} year='numeric' month='short' day='2-digit' /></div>
         </div>
+        {isBindable && <Button
+          bsSize='small'
+          onClick={() => addBinding(file)}></Button>}
       </div>
     );
   }
