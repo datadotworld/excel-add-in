2ec1fb3c095e07d02dc86a41d53eb1f02f714b3f
Alan Levicki <alevicki@users.noreply.github.com>
2018-02-21 09:46:04 -0600

    Revert "Upload charts to data.world as insights (#62)" (#63)
    This reverts commit d5617e40f3e7ebaead96bb6127ff54016f409926.

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -24,23 +24,28 @@ import {
   Row
 } from 'react-bootstrap';
 
-import LogoStacked from './icons/LogoStacked';
-import insightsIcon from '../static/img/icon_insight_round.svg';
-import datasetIcon from '../static/img/icon_dataset_round.svg';
-import importIcon from '../static/img/icon_import_round.svg';
-
 import './WelcomePage.css';
+import LogoStacked from './icons/LogoStacked';
+import DatasetItem from './DatasetItem';
 import analytics from '../analytics';
 
 class WelcomePage extends Component {
 
   static propTypes = {
-    page: PropTypes.string
+    dataset: PropTypes.object
   }
 
-  constructor (props) {
+  constructor () {
     super();
-    this.oauthURI = `${process.env.REACT_APP_OAUTH_URI}?page=${props.page}`;
+    this.oauthURI = process.env.REACT_APP_OAUTH_URI;
+  }
+
+  componentWillMount = () => {
+    document.body.style.backgroundColor = '#335c8c';
+  }
+  
+  componentWillUnmoun = () => {
+    document.body.style.backgroundColor = null;
   }
 
   startAuthFlow = () => {
@@ -49,7 +54,6 @@ class WelcomePage extends Component {
 
   loginClick = () => {
     analytics.track('exceladdin.welcome.login.click');
-    this.startAuthFlow();
   }
 
   signUpClick = () => {
@@ -57,53 +61,29 @@ class WelcomePage extends Component {
   }
 
   render () {
+    const {dataset} = this.props;
     return (
       <Grid className='welcome-page'>
         <Row className='center-block'>
           <LogoStacked />
-          <div className="feature">
-            <img
-              alt="insights logo"
-              src={insightsIcon}
-              className="feature-icon"
-            />
-            <p className="feature-text">Publish charts as insights</p>
-          </div>
-          <div className="feature">
-            <img
-              alt="insights logo"
-              src={datasetIcon}
-              className="feature-icon"
-            />
-            <p className="feature-text">Save your data on data.world</p>
-          </div>
-          <div className="feature">
-            <img
-              alt="insights logo"
-              src={importIcon}
-              className="feature-icon"
-            />
-            <p className="feature-text">Import datasets into Excel</p>
-          </div>
+          {!dataset && <div>
+            <div className='description'>
+              <p>Turn Excel files into<br />data.world datasets</p>
+              <p>Collaborate with<br />other Excel users</p>
+              <p>Sync changes to data.world<br />with a single click</p>
+            </div>
+          </div>}
+          {dataset && <div>
+            <div>This file is linked to a dataset on data.world:</div>
+            <DatasetItem dataset={dataset} buttonText='View' buttonLink={`https://data.world/${dataset.owner}/${dataset.id}`} />
+            <div className='message'>Sign in to data.world to save changes as they are made.</div>
+          </div>}
           <Button
             className='center-block login-button'
+            href={this.oauthURI}
             onClick={this.loginClick}
-            bsStyle='primary'
-          >
-            Sign in
-          </Button>
-          <div>
-            {`New to data.world? `}
-            <a
-              href='https://data.world/'
-              target='_blank'
-              rel='noopener noreferrer'
-              onClick={this.signUpClick}
-              className="sign-up-link"
-            >
-              Sign up now
-            </a>
-          </div>
+            bsStyle='primary'>Sign in</Button>
+          <div>New to data.world? <a href='https://data.world/' target='_blank' rel='noopener noreferrer' onClick={this.signUpClick}> Sign up now</a></div>
         </Row>
       </Grid>
     );
