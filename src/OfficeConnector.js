c0dbc41bc075d12adc9bfb6e775663589cb092b1
Patrick Zhang <patrick.zhang@data.world>
2018-08-24 14:12:00 -0500

    lint fixes

--- src/OfficeConnector.js
+++ src/OfficeConnector.js
@@ -32,7 +32,7 @@ const lastRowsAreEmpty = function (array) {
 
 const generateUUID = function () {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    var r = Math.random() * 16 | 0, v = c === 'x' ? r : ((r & 0x3) | 0x8);
     return v.toString(16);
   });
 }
@@ -93,7 +93,6 @@ export default class OfficeConnector {
       Excel.run((ctx) => {
         Office = window.Office
         const uuid = Office.context.document.settings.get('WorkbookId')
-        console.log("uuid", uuid)
         return ctx.sync().then(() => {
           resolve(uuid)
         }).catch(reject)
