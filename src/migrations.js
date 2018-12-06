6c167fbb81ec5576dfaaff28fa5f535ca0755dd6
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2018-12-06 19:40:43 +0300

    Update bindings migration

--- src/migrations.js
+++ src/migrations.js
@@ -1,16 +1,24 @@
-const migrateBindings = ({
+const migrateBindings = async ({
   dataset,
   bindings,
   pushToLocalStorage,
   getSheetId
 }) => {
+  // Remove previous migrations if any
+  localStorage.removeItem('history');
+
+  // Run migrations
   const datasetFiles = dataset.files.map((entry) => entry.name);
-  bindings.forEach(async (binding) => {
+  const worksheetIds = await Promise.all(
+    bindings.map((binding) => getSheetId(binding.rangeAddress))
+  );
+
+  bindings.forEach(async (binding, index) => {
     const fileName = binding.id.replace('dw::', '');
-    const worksheetId = await getSheetId(binding.rangeAddress);
+    const worksheetId = worksheetIds[index];
 
     if (datasetFiles.includes(fileName)) {
-      pushToLocalStorage(
+      await pushToLocalStorage(
         { owner: dataset.owner, id: dataset.id },
         fileName,
         binding.rangeAddress,
